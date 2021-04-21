665a637fcb70a9bd627480ebb22290eb7609cbf2
victor-singha <vicunite2@gmail.com>
2021-04-21 17:43:29 +0530

    initial commit

---
+++ NLogDemo/NLog.cs
@@ -0,0 +1,28 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLogDemo
+{
+    public class NLog
+    {
+        public Logger logger = LogManager.GetCurrentClassLogger();
+        public void LogDebug(string input)
+        {
+            logger.Debug(input);
+        }
+        public void LogError(string input)
+        {
+            logger.Error(input);
+        }
+        public void LogWarn(string input)
+        {
+            logger.Warn(input);
+        }
+        public void LogInfo(string input)
+        {
+            logger.Info(input);
+        }
+    }
+}
